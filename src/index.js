20f5a5fe9db2e5b19699cec8c1177099208dd733
Vanya Maharani <Vanyamhrni@gmail.com>
2023-04-09 22:07:55 +0700

    update_products&users

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// import App from './App';
+import AppRoute from './Pertemuan-4/apps/AppRoute';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Form from './components/Form';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+const {PUBLIC_URL} = process.env;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
-    <Form />
+    <BrowserRouter basename={PUBLIC_URL}>
+      <AppRoute />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
